ec2ddb0fcbcd31673793dea3d963e012d19803a8
inngutorova <inngutorova@gmail.com>
2021-10-20 00:13:11 +0300

    vcs

--- demo/src/main/java/com/example/demo/ApiController.java
+++ demo/src/main/java/com/example/demo/ApiController.java
@@ -1,14 +1,43 @@
 package com.example.demo;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ApiController {
-    @GetMapping("books")
-    public ResponseEntity<String> getText() {
-        return ResponseEntity.ok("Hello text");
+
+    private List<User> users = new ArrayList<>();
+
+    @GetMapping("users")
+    public List<User> getUsers() {
+        return users;
     }
+
+    // curl -X POST http://localhost:8080/users -H 'Content-Type: application/json' -d '{"name" : "Oleg", "age" : "56"}' - добавляет сообщение в список
+
+    @PostMapping("users")
+    public void addUser(@RequestBody User user) {
+       users.add(user);
+    }
+
+    // curl -X GET http://localhost:8080/users/0 - выводит на экран пользователя по индексу
+    @GetMapping("users/{index}")
+    public User getUser(@PathVariable("index") Integer index) {
+        return users.get(index);
+    }
+    // curl -X DELETE http://localhost:8080/users/0 - удаляет пользователя по индексу
+    @DeleteMapping("users/{index}")
+    public void deleteUser(@PathVariable("index") Integer index) {
+        users.remove((int) index);
+    }
+
+    // curl -X PUT http://localhost:8080/users/0 -H 'Content-Type: application/json' -d '2' - обновляет возраст пользователя по индексу
+
+    @PutMapping("users/{index}")
+    public void updateUserAge(@PathVariable("index") Integer i, @RequestBody int age) {
+        users.get(i).setAge(age);
+    }
+    // curl -X GET http://localhost:8080/users - выводит список пользователей
 }
+
